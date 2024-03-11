3a20725c4c66db8ba64dfa4cb087f8d5bef54559
Juanpfrancos <juanpanime@gmail.com>
2024-03-10 23:03:50 -0500

    Add add app to project

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_spectacular'
+    'drf_spectacular',
+    'app'
 ]
 
 
